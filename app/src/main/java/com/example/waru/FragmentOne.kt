467f36ea83dcbbde67f7ad8fe4e4c0e2c7022a1b
moondev03 <moondev03@gmail.com>
2023-06-07 18:49:20 +0900

    ProgressBar 제거 / TextView로 대체해서 동적으로 작동하게 함

--- app/src/main/java/com/example/waru/FragmentOne.kt
+++ app/src/main/java/com/example/waru/FragmentOne.kt
@@ -4,13 +4,11 @@ import android.annotation.SuppressLint
 import android.database.Cursor
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
 import android.provider.BaseColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.waru.databinding.ActivitySub22Binding
 
 class FragmentOne(private val date: String) :Fragment() {
@@ -29,39 +27,70 @@ class FragmentOne(private val date: String) :Fragment() {
         isDbHelperInitialized = true
 
         //db에 저장되어 있을 때(분석결과 보고 다시 코멘트 보려고 할 때)는 로딩중이 뜨면 안되므로 경우 나눠줌
-        //progressbar 이용하여 로딩을 만들어 openAI의 결과가 db에 저장될 때 까지 로딩중이 뜨도록 만듦
+        //progressbar 이용하여 로딩을 만들어 openAI의 결과가 db에 저장될 때 까지 로딩중이 뜨도록 만듦 (제거)
+        // textview를 animation를 사용해 DB에 저장될 때 까지 로딩화면이 출력되도록 구현함 (수정)
+        val loading = LoadingAni(binding.lodingTextView)
         val hasDiary = checkDiaryExists(date)
+
         if (hasDiary) {
+            loading.stopAnimation()
             val comment = readComment(date)
             binding.comment.text = comment
         } else {
-            // 로딩 중 뷰 표시
-            showLoadingView()
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                val comment = readComment(date)
-                binding.comment.text = comment
-
-                // 로딩 중 뷰 숨기기
-                hideLoadingView()
-            }, 10000) //10초 설정
+            loading.startLoding()
+            startCheckingDiaryExists(loading)
         }
+//        if (hasDiary) {
+//            val comment = readComment(date)
+//            binding.comment.text = comment
+//        } else {
+            // 로딩 중 뷰 표시
+//            showLoadingView()
+
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                val comment = readComment(date)
+//                binding.comment.text = comment
+//
+//                // 로딩 중 뷰 숨기기
+//                hideLoadingView()
+//            }, 10000) //10초 설정
+//        }
+
+
 
 
     }
 
-    private fun showLoadingView() {
-        binding.progressBar.visibility = View.VISIBLE
-        binding.comment.visibility = View.GONE
+//    private fun showLoadingView() {
+//        binding.progressBar.visibility = View.VISIBLE
+//        binding.comment.visibility = View.GONE
+//    }
+//
+//    private fun hideLoadingView() {
+//        binding.progressBar.visibility = View.GONE
+//        binding.comment.visibility = View.VISIBLE
+//    }
+
+    private fun startCheckingDiaryExists(loading: LoadingAni) {
+        val handler = Handler()
+        val delay = 1000L // 1초마다 체크
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val hasDiary = checkDiaryExists(date)
+                if (hasDiary) {
+                    loading.stopAnimation()
+                    val comment = readComment(date)
+                    binding.comment.text = comment
+                } else {
+                    handler.postDelayed(this, delay)
+                }
+            }
+        }, delay)
     }
 
-    private fun hideLoadingView() {
-        binding.progressBar.visibility = View.GONE
-        binding.comment.visibility = View.VISIBLE
-    }
 
     private fun checkDiaryExists(date: String?): Boolean {
-        dbHelper = Database.DbHelper(requireContext())
         val db = dbHelper.readableDatabase
 
         val projection = arrayOf(BaseColumns._ID)
---
+++ app/src/main/java/com/example/waru/LoadingAni.kt
@@ -0,0 +1,43 @@
+package com.example.waru
+
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoadingAni(private val loadingTextView: TextView) : AppCompatActivity() {
+//    private lateinit var loadingTextView: TextView
+    private lateinit var animation: AlphaAnimation
+
+
+    private fun animateLoadingText() {
+        animation = AlphaAnimation(0.0f, 1.0f).apply {
+            duration = 1000 // 애니메이션 지속 시간 (밀리초)
+            repeatCount = Animation.INFINITE // 무한 반복 설정
+            repeatMode = Animation.REVERSE // 반복 모드 설정 (역방향으로 반복)
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                    loadingTextView.visibility = TextView.VISIBLE
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                    loadingTextView.text = "WARU가 코멘트를 생성하는 중..."
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    loadingTextView.visibility = TextView.GONE
+                }
+            })
+        }
+
+        loadingTextView.startAnimation(animation)
+    }
+
+    fun stopAnimation() {
+        animation.cancel()
+    }
+
+    fun startLoding(){
+        animateLoadingText()
+    }
+}
