e9a7e8dc32e1282decf61d9dd66e70734d0ab635
GyuriKim12 <kralicia@naver.com>
2023-06-12 00:43:12 +0900

    fragmentOne 관련

--- app/src/main/java/com/example/waru/FragmentOne.kt
+++ app/src/main/java/com/example/waru/FragmentOne.kt
@@ -18,7 +18,7 @@ class FragmentOne(private val date: String) :Fragment() {
     private lateinit var dbHelper: Database.DbHelper
     private var isDbHelperInitialized = false
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = ActivitySub22Binding.inflate(inflater, container, false)
         return binding.root
     }
@@ -28,36 +28,55 @@ class FragmentOne(private val date: String) :Fragment() {
         dbHelper = Database.DbHelper(requireContext())  // dbHelper 초기화
         isDbHelperInitialized = true
 
-        //db에 저장되어 있을 때(분석결과 보고 다시 코멘트 보려고 할 때)는 로딩중이 뜨면 안되므로 경우 나눠줌
-        //progressbar 이용하여 로딩을 만들어 openAI의 결과가 db에 저장될 때 까지 로딩중이 뜨도록 만듦
+        val loading = LoadingAni(binding.lodingTextView)
         val hasDiary = checkDiaryExists(date)
         if (hasDiary) {
+            loading.stopAnimation()
             val comment = readComment(date)
-            binding.comment.text = comment
+            binding.lodingTextView.text = comment
         } else {
-            // 로딩 중 뷰 표시
-            showLoadingView()
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                val comment = readComment(date)
-                binding.comment.text = comment
-
-                // 로딩 중 뷰 숨기기
-                hideLoadingView()
-            }, 10000) //10초 설정
+            loading.startLoding()
+            startCheckingDiaryExists(loading)
         }
 
+        //db에 저장되어 있을 때(분석결과 보고 다시 코멘트 보려고 할 때)는 로딩중이 뜨면 안되므로 경우 나눠줌
+        //progressbar 이용하여 로딩을 만들어 openAI의 결과가 db에 저장될 때 까지 로딩중이 뜨도록 만듦
+//        val hasDiary = checkDiaryExists(date)
+//        if (hasDiary) {
+//            val comment = readComment(date)
+//            binding.comment.text = comment
+//        } else {
+//            // 로딩 중 뷰 표시
+//            showLoadingView()
+//
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                val comment = readComment(date)
+//                binding.comment.text = comment
+//
+//                // 로딩 중 뷰 숨기기
+//                hideLoadingView()
+//            }, 10000) //10초 설정
+//        }
+
 
     }
 
-    private fun showLoadingView() {
-        binding.lodingTextView.visibility = View.VISIBLE
-        binding.comment.visibility = View.GONE
-    }
+    private fun startCheckingDiaryExists(loading: LoadingAni) {
+        val handler = Handler()
+        val delay = 1000L // 1초마다 체크
 
-    private fun hideLoadingView() {
-        binding.lodingTextView.visibility = View.GONE
-        binding.comment.visibility = View.VISIBLE
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val hasDiary = checkDiaryExists(date)
+                if (hasDiary) {
+                    loading.stopAnimation()
+                    val comment = readComment(date)
+                    binding.comment.text = comment
+                } else {
+                    handler.postDelayed(this, delay)
+                }
+            }
+        }, delay)
     }
 
     private fun checkDiaryExists(date: String?): Boolean {
